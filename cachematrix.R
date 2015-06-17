1066905f255b7b1ed1c342a0f1de4650a7101887
Nitin Mundhe <nitinmundhe@gmail.com>
2015-06-17 14:09:08 +0530

    Completed assignment for rprog-015 assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Matrix inversion is usually a costly computation and there may be some benefit 
+#to caching the inverse of a matrix rather than compute it repeatedly 
+#I have written a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function receives the matrix as input and is provides getter and setter method for that matrix and its inverse
 
+makeCacheMatrix <- function(mat = matrix()) {
+  matrixInverse <- NULL
+  get <- function() mat
+  set <- function(newMat) {
+    mat <<- newMat
+    matrixInverse <<- NULL
+  }
+  setMatrixInverse <- function(inverse) matrixInverse <<- solve(inverse)
+  getMatrixInverse <- function() matrixInverse  
+  list(set = set, get = get,
+       setMatrixInverse = setMatrixInverse,
+       getMatrixInverse = getMatrixInverse)
 }
 
 
-## Write a short comment describing this function
+## This function receives the object of the makeCacheMatrix as its input and return invrse of matrix from its chache if exist else first cache it and then return the inverse
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(mCM, ...) {
+  matrixInverse <- mCM$getMatrixInverse()
+  if(!is.null(matrixInverse)) {
+    message("getting cached Matrix Inverse")
+    return(matrixInverse)
+  }  
+  data <- mCM$get()
+  matrixInverse <- solve(data)
+  mCM$setMatrixInverse(matrixInverse)
+  matrixInverse  
         ## Return a matrix that is the inverse of 'x'
+  
 }
